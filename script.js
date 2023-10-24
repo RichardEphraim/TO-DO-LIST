743e9f7bb90d25942d20f364cbd31897f97bc9d0
RichardEphraim <makindeayomide2024@gmail.com>
2023-10-24 16:09:41 +0100

    new change

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const tasks = document.querySelector("#tasks")
 const addTask = document.querySelector("#add")
 const addnew = document.querySelector("#new")
 
-addTask.addEventListener("click", function(){
+function changeStyle(){
     const newInput = document.createElement("li")
     newInput.textContent = addnew.value
     tasks.appendChild(newInput)
     addnew.value = ""
-})
+}
+
+addTask.addEventListener("click", changeStyle)
