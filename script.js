efabc615453c3f4c309e6f90c14e5b5d6638e334
RichardEphraim <makindeayomide2024@gmail.com>
2023-10-25 14:02:47 +0100

    new changes

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const tasks = document.querySelector("#tasks")
-const addTask = document.querySelector("#add")
-const addnew = document.querySelector("#new")
+// const tasks = document.querySelector("#tasks")
+// const addTask = document.querySelector("#add")
+// const addnew = document.querySelector("#new")
 
-function changeStyle(){
-    const newInput = document.createElement("li")
-    newInput.textContent = addnew.value
-    tasks.appendChild(newInput)
-    addnew.value = ""
-}
+// function changeStyle(){
+//     const newInput = document.createElement("li")
+//     newInput.textContent = addnew.value
+//     tasks.appendChild(newInput)
+//     addnew.value = ""
+// }
 
-addTask.addEventListener("click", changeStyle)
+// addTask.addEventListener("click", changeStyle)
+const button = document.querySelector(".theme");
+
+button.addEventListener("click", () => {
+  const cssLink = document.querySelector("link[rel='stylesheet']");
+  cssLink.href = "day.css";
+});
