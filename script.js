9493e267176f24de7af8cd64c4039b2700d5ad6c
RichardEphraim <makindeayomide2024@gmail.com>
2023-10-26 09:11:34 +0100

    new changes

--- script.js
+++ script.js
@@ -11,8 +11,21 @@
 
 // addTask.addEventListener("click", changeStyle)
 const button = document.querySelector(".theme");
+const cssLink = document.querySelector("link[rel='stylesheet']");
+
+
+let currentCssFile = "style.css";
+
 
 button.addEventListener("click", () => {
-  const cssLink = document.querySelector("link[rel='stylesheet']");
-  cssLink.href = "day.css";
-});
+this.classList.button('theme1')
+
+//   if (currentCssFile === "style.css") {
+//     cssLink.href = "day.css";
+//     currentCssFile = "day.css";
+//   } 
+//   else {
+//     cssLink.href = "style.css";
+//     currentCssFile = "style.css";
+//   }
+})
