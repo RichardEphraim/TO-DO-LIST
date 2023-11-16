c45b17ad8cfab81d6a274089f2aca36d736e6587
RichardEphraim <makindeayomide2024@gmail.com>
2023-11-16 19:18:12 +0100

    comit

--- functions.js
+++ functions.js
@@ -2,6 +2,21 @@ const tasksInput = document.querySelector("#new");
 const addTaskButton = document.querySelector(".button");
 const itemWrapper = document.querySelector(".wrap");
 
+const storedTask = JSON.parse(localStorage.getItem("tasks")) || [];
+
+storedTask.forEach(function(tasks){
+itemWrapper.innerHTML += `<div id="tasks">
+<div class="inputs">
+    <input type="checkbox" id="todo">
+    <label class="slash" for="todo"> ${tasks.name}</label>
+</div>
+<div>
+    <button class="remove-btn"><i class="fa-solid fa-trash"></i></button>
+</div>
+</div>`;
+
+});
+
 addTaskButton.addEventListener("click", function() {
 const newInput = tasksInput.value.trim();
 if (newInput === "") {
@@ -18,14 +33,14 @@ itemWrapper.innerHTML += `<div id="tasks">
 </div>
 </div>`;
 
+storedTask.push({name:newInput})
+localStorage.setItem("tasks", JSON.stringify(storedTask))
 
-const removeBtn = document.querySelectorAll(".remove-btn");
+};
 
-removeBtn.forEach((btn) => {
-    btn.addEventListener("click", function () {
-        btn.parentNode.parentNode.remove()
-      })
-});
+tasksInput.value = ""
+}
+);
 
 
 const slash = document.querySelectorAll('input[type="checkbox"]');
@@ -41,10 +56,22 @@ slash.forEach((checkbox, index) => {
     });
 });
 
-};
-
-tasksInput.value = ""
-}
-);
 
 
+const removeBtn = document.querySelectorAll(".remove-btn");
+
+removeBtn.forEach((btn) => {
+    btn.addEventListener("click", function () {
+        btn.parentNode.parentNode.remove();
+
+        // Find the corresponding task in storedTask array
+        const taskNameToRemove = btn.parentNode.parentNode.querySelector('.slash').textContent;
+        const taskIndex = storedTask.findIndex(task => task.name === taskNameToRemove);
+
+        // Remove task from storedTask array
+        storedTask.splice(taskIndex, 1);
+
+        // Update localStorage
+        localStorage.setItem("tasks", JSON.stringify(storedTask));
+    });
+})
