2bf2c1274aba89501f81f7e40d44cea1d23f5a44
RichardEphraim <makindeayomide2024@gmail.com>
2023-11-18 18:33:19 +0100

    new intake

--- functions.js
+++ functions.js
@@ -1,77 +1,77 @@
-const tasksInput = document.querySelector("#new");
-const addTaskButton = document.querySelector(".button");
-const itemWrapper = document.querySelector(".wrap");
+// const tasksInput = document.querySelector("#new");
+// const addTaskButton = document.querySelector(".button");
+// const itemWrapper = document.querySelector(".wrap");
 
-const storedTask = JSON.parse(localStorage.getItem("tasks")) || [];
+// const storedTask = JSON.parse(localStorage.getItem("tasks")) || [];
 
-storedTask.forEach(function(tasks){
-itemWrapper.innerHTML += `<div id="tasks">
-<div class="inputs">
-    <input type="checkbox" id="todo">
-    <label class="slash" for="todo"> ${tasks.name}</label>
-</div>
-<div>
-    <button class="remove-btn"><i class="fa-solid fa-trash"></i></button>
-</div>
-</div>`;
+// storedTask.forEach(function(tasks){
+// itemWrapper.innerHTML += `<div id="tasks">
+// <div class="inputs">
+//     <input type="checkbox" id="todo">
+//     <label class="slash" for="todo"> ${tasks.name}</label>
+// </div>
+// <div>
+//     <button class="remove-btn"><i class="fa-solid fa-trash"></i></button>
+// </div>
+// </div>`;
 
-});
+// });
 
-addTaskButton.addEventListener("click", function() {
-const newInput = tasksInput.value.trim();
-if (newInput === "") {
-alert("empty todo");
-}
-else {
-itemWrapper.innerHTML += `<div id="tasks">
-<div class="inputs">
-    <input type="checkbox" id="todo">
-    <label class="slash" for="todo"> ${newInput}</label>
-</div>
-<div>
-    <button class="remove-btn"><i class="fa-solid fa-trash"></i></button>
-</div>
-</div>`;
+// addTaskButton.addEventListener("click", function() {
+// const newInput = tasksInput.value.trim();
+// if (newInput === "") {
+// alert("empty todo");
+// }
+// else {
+// itemWrapper.innerHTML += `<div id="tasks">
+// <div class="inputs">
+//     <input type="checkbox" id="todo">
+//     <label class="slash" for="todo"> ${newInput}</label>
+// </div>
+// <div>
+//     <button class="remove-btn"><i class="fa-solid fa-trash"></i></button>
+// </div>
+// </div>`;
 
-storedTask.push({name:newInput})
-localStorage.setItem("tasks", JSON.stringify(storedTask))
+// storedTask.push({name:newInput})
+// localStorage.setItem("tasks", JSON.stringify(storedTask))
 
-};
+// };
 
-tasksInput.value = ""
-}
-);
-
-
-const slash = document.querySelectorAll('input[type="checkbox"]');
-const label = document.querySelectorAll('.slash');
-
-slash.forEach((checkbox, index) => {
-    checkbox.addEventListener("click", () => {
-        if (checkbox.checked) {
-            label[index].classList.add("strike");
-        } else {
-            label[index].classList.remove("strike");
-        }
-    });
-});
+// tasksInput.value = ""
+// }
+// );
 
 
+// const slash = document.querySelectorAll('input[type="checkbox"]');
+// const label = document.querySelectorAll('.slash');
 
-const removeBtn = document.querySelectorAll(".remove-btn");
+// slash.forEach((checkbox, index) => {
+//     checkbox.addEventListener("click", () => {
+//         if (checkbox.checked) {
+//             label[index].classList.add("strike");
+//         } else {
+//             label[index].classList.remove("strike");
+//         }
+//     });
+// });
 
-removeBtn.forEach((btn) => {
-    btn.addEventListener("click", function () {
-        btn.parentNode.parentNode.remove();
 
-        // Find the corresponding task in storedTask array
-        const taskNameToRemove = btn.parentNode.parentNode.querySelector('.slash').textContent;
-        const taskIndex = storedTask.findIndex(task => task.name === taskNameToRemove);
 
-        // Remove task from storedTask array
-        storedTask.splice(taskIndex, 1);
+// const removeBtn = document.querySelectorAll(".remove-btn");
 
-        // Update localStorage
-        localStorage.setItem("tasks", JSON.stringify(storedTask));
-    });
-})
+// removeBtn.forEach((btn) => {
+//     btn.addEventListener("click", function () {
+//         btn.parentNode.parentNode.remove();
+
+//         // Find the corresponding task in storedTask array
+//         const taskNameToRemove = btn.parentNode.parentNode.querySelector('.slash').textContent;
+//         const taskIndex = storedTask.findIndex(task => task.name === taskNameToRemove);
+
+//         // Remove task from storedTask array
+//         storedTask.splice(taskIndex, 1);
+
+//         // Update localStorage
+//         localStorage.setItem("tasks", JSON.stringify(storedTask));
+//     });
+// })
